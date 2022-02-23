9538e17c795f84683ba0dd8acd98a7b963c45358
Alejandra Landa <aleriquene@gmail.com>
2022-02-22 18:42:00 -0600

    commit tree a0950f0376cfdc460ef395e07f2eb01f93773ee3

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+//4
+{
+const max = (1,3, 20)
+console.log(Math.max(...max));}
+
+
+//1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+console.log(multiply)
+
+//2
+let random = Math.floor(Math.random() * 10) + 1;
+
+//3
+let num3 = 22
+let num4 = 6
+let mod = num3 % num4;
+console.log(mod)
